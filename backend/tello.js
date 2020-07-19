23bb51516e49681460b9e3d4be4bdebf77258a1c
sitgofour <evansmithcodes@gmail.com>
2020-07-18 19:04:26 -0700

    flight data communicating now

--- backend/tello.js
+++ backend/tello.js
@@ -6,7 +6,7 @@ const dgram = require('dgram');
 const HOST = "192.168.10.1";
 const PORT = 8889;
 const tello = dgram.createSocket('udp4');
-
+let count = 0;
 
 
 tello.on("error", function (err) {
@@ -15,15 +15,9 @@ tello.on("error", function (err) {
   });
   
   tello.on("message", function (msg, rinfo) {
-    setTimeout(function() {
-
-        // ON Command response from tello,
-        // emit message to react client
         socket.emit('commandResponse', msg);
-
         console.log("server got: " + msg + " from " +
         rinfo.address + ":" + rinfo.port);
-    }, 5000);
   });
   
   tello.on("listening", function () {
@@ -35,7 +29,13 @@ tello.on("error", function (err) {
   tello.bind(PORT);
 
 
+
+
+
+
+////////////////////////////////////////////////  
 // command functions
+////////////////////////////////////////////////  
 
 const handleErrror = (error, errorMessage) => {
   if(error) {
@@ -53,7 +53,12 @@ const flightCommand = (command) => {
 
 
 
+
+
+
+////////////////////////////////////////////////  
 // tello state commands stream
+////////////////////////////////////////////////  
 
 const telloState = dgram.createSocket('udp4');
 
@@ -63,9 +68,18 @@ telloState.on("error", function (err) {
 });
 
 telloState.on("message", function (msg, rinfo) {
-  setInterval(function() {
-    console.log("server got: " + msg + " from TelloState" + rinfo.address + ":" + rinfo.port);
-  }, 5000);
+  /* When you get flight info from tello here, forward it to 
+    front-end using port 8001 below */
+
+  //hacky way to throttle?
+  if(count % 5 === 0) {
+    
+    sendTelloDataToClient(msg);
+    count++;
+
+  } else {
+    count++;
+  }
 });
 
 telloState.on("listening", function () {
@@ -115,6 +129,22 @@ const socket = socketIO(server);
 socket.on('connect', socket => {
   console.log('connected to socket on 8001');
 });
+
+/* ****************************************************** */
+
+function sendTelloDataToClient(data) {
+  socket.emit("tello data", data);
+}
+
+/* ****************************************************** */
+
+// function convertBufferToString(bufferObject) {
+
+//   let dataView = new DataView(bufferObject);
+//   let decoder = new TextDecoder("utf-8");
+
+//   return decoder.decode(dataView);
+// }
 
 /* ****************************************************** */
 
--- tello/src/components/StreamData.js
+++ tello/src/components/StreamData.js
@@ -1,29 +1,35 @@
 import React, {Component} from 'react';
 import socketIOClient from "socket.io-client";
+const socket = socketIOClient('http://localhost:8001/');
+
+function telloDataSocket(cb) {
+    socket.on("tello data", (data) => cb(data)); 
+}
 
 
 class StreamData extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            "telloData": ""
+            telloData: "Data",
         }
-    }
-    render() {
-
-        const socket = socketIOClient('http://localhost:8000/');
-
-        socket.on('news', data => {
+        telloDataSocket((data) => {
+            let dataView = new DataView(data);
+            let decoder = new TextDecoder("utf-8");
+            let newData = decoder.decode(dataView);
             this.setState({
-                "telloData": data
+                telloData: newData
             });
         });
+    }
 
+
+    render() {
 
         return (
             <div>
-                <h2>stream data</h2>
-                <p>{this.state.data}</p>
+                <h2>Stream data</h2>
+                <p>{this.state.telloData}</p>
             </div>
         )
     }
